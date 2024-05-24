c35f37b24239d49d98dd5044296d0e4ebaeb377d
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-24 11:12:00 +0300

    Creating a RequestParameters folder for Pagination Operations

---
+++ PayMaster.Application/RequestParameters/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayMaster.Application.RequestParameters
+{
+    public class Pagination
+    {
+        public int PageCount { get; set; } = 0;
+        public int PageSize { get; set; } = 5;
+    }
+}
