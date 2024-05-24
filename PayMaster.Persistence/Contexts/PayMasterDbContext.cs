f045df89c1e2504686720e3c06eb039b9a8a7c94
Aleyna ÇELİK <celikaleyna71@gmail.com>
2024-05-24 11:45:42 +0300

    Creating the DbContext Class

---
+++ PayMaster.Persistence/Contexts/PayMasterDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayMaster.Domain.Entities;
+
+namespace PayMaster.Persistence.Contexts
+{
+    public class PayMasterDbContext : DbContext
+    {
+        public PayMasterDbContext(DbContextOptions<PayMasterDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+
+        //Override işlemleri gelebilir
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Ek yapılandırmalar burada yapılabilir
+        }
+    }
+}
